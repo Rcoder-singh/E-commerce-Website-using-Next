b29c51357fa3233db2100e3104b18bb347172815
Rcoder-singh <rishabh22.singh@gmail.com>
2023-10-29 00:08:19 +0530

    New Arrival Section

---
+++ src/Components/NewArrival.jsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState } from "react";
+import { Whisper } from "next/font/google";
+
+const whisper = Whisper({ subsets: ["latin"], weight: ["400"] });
+
+const tabsData = ["All", "Skin Care", "Lipsticks", "Makeup", "Nail & Wax"];
+
+const NewArrival = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTab = (index) => {
+    setSelectedTab(index);
+  };
+
+  return (
+    <div className="container pt-32">
+      <div className="text-center">
+        <h3 className={`${whisper.className} text-[40px] text-gray-500`}>
+          For your Beauty
+        </h3>
+        <h2 className="font-semibold text-5xl">New Arrival</h2>
+        <ul className="flex flex-col sm:flex-row gap-4 sm:gap-8 justify-center pt-8 uppercase font-medium text-xl">
+          {tabsData.map((text, index) => (
+            <li
+              key={text}
+              className={`${
+                selectedTab === index && "text-accent"
+              } cursor-pointer hover:text-accent`}
+              onClick={() => handleTab(index)}
+            >
+              {text}
+            </li>
+          ))}
+        </ul>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 pt-8"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NewArrival;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Feature from "@/Components/Feature";
 import Hero from "@/Components/Hero";
+import NewArrival from "@/Components/NewArrival";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Feature />
+      <NewArrival />
     </main>
   );
 }
